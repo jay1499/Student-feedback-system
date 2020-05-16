f36ca740cf72bb75ff693dacac5d1741705a919e
jay1499 <jayanth.cs17@bmsce.ac.in>
2020-05-16 12:19:51 +0530

    npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#login").click(function(){
+        $('#loginModal').modal('show');
+
+    });
+
+    $("#formclick").click(function(){
+        $('#reserveform').modal('show');
+
+    });
+
+    $("#closereserve").click(function(){
+        $('#reserveform').modal('hide');
+
+    });
+
+});
+
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval:2000});
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
